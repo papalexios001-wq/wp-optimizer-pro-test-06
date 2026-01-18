c18a180d59e928cd9f9b9c7ed42f0dfa5d40b1f2
papalexios001-wq <papalexios001@gmail.com>
2026-01-18 11:43:42 +0200

    fix: add missing commas in export default object (TS1005)

--- src/index.ts
+++ src/index.ts
@@ -183,11 +183,11 @@ export default {
   VERSION,
   APP_NAME,
   DEFAULT_CONFIG,
-  apiService: apiService
-  contentEngine: contentEngine
-  store: store
-  actions: actions
-  selectors: selectors
+  apiService: apiService,
+  contentEngine: contentEngine,
+  store: store,
+  actions: actions,
+  selectors: selectors,
   initialize,
   generateContent,
 };
